e4aea2f3c76c95a28e5448556739dd571c8e4009
zseaborn63 <zseaborn63@gmail.com>
2016-02-17 16:09:37 -0500

    Basic program is working

--- list_overlap.py
+++ list_overlap.py
@@ -1,2 +1,9 @@
-print("hello world")
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
+new_list = []
+for x in a and b:
+    if x in a and b:
+        new_list.append(x)
+
+print(new_list)
